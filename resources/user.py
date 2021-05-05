4991929f1f64ddc5444096ac170b12c987eee0b9
sharexy23 <66343719+sharexy23@users.noreply.github.com>
2021-05-05 05:42:52 -0700

    Update user.py

--- resources/user.py
+++ resources/user.py
@@ -9,10 +9,8 @@ from werkzeug.utils import secure_filename
 from io import BytesIO
 
 
-
 class register(Resource):
     parser = reqparse.RequestParser()
-
     parser.add_argument('username',
                         type=str,
                         required=True,
@@ -28,28 +26,19 @@ class register(Resource):
                         required=True,
                         help="This field cannot be left blank!"
                         )
-
     def encrypt_string(hash_string):
         sha_signature = \
             hashlib.sha256(hash_string.encode()).hexdigest()
         return sha_signature
-
-
     def post(self):
-
         data = register.parser.parse_args()
         if User.find_by_email(data['email']):
             return  {
             'status':False,
             'message':'user exists'
             },400
-
         user = User(data['username'],data['email'],data['password'])
-
         User.save_to_db(user)
-
-
-
         #notification.notify(title= "notification",message="you are succesfully registered",timeout=5)
         return {
         'status': True,
@@ -57,8 +46,6 @@ class register(Resource):
         'data':user.json(),
         'message':'user created succesfully'
         },201
-
-
 class login(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('email',
@@ -85,201 +72,11 @@ class login(Resource):
                 },200
             except:
                 {'message':'a little error occuredwhen trying to access the database'}
-
-
         return {
         'status':True,
         'status':False,
         'message':'user not found'
         },404
-class top_up(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('username',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
-    parser.add_argument('password',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
-    parser.add_argument('ammount',
-                        type=int,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
-    def post(self):
-        data = top_up.parser.parse_args()
-        user = User.find_by_username(data['username']) and User.find_by_password(data['password'])
-        if user:
-            user.account_balance = Integer(user.account_balance)
-            user.account_balance = user.account_balance + data['ammount']
-            user.account_balance = string(user.account_balance)
-            try:
-                return {
-                      'status': True,
-                      'data': user.account_balance,
-                      'message':'your account_balance'
-                },200
-            except:
-                {'message':'a little error occured when trying to access the database'}
-
-
-        return {
-        'status':True,
-        'status':False,
-        'message':'either your username or password is incorrect'
-        },404
-
-class add_to_wishlist(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('Product_name',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
-    parser.add_argument('username',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
-    def post(self):
-        data = add_to_wishlist.parser.parse_args()
-        Product = Product.find_by_name(data['Product_name'])
-        user = User.find_by_username(data['username'])
-        if product:
-            wishlist = Wishlist(data['Product_name'],product.price,user.id)
-            Wishlist.save_to_db(wishlist)
-            return{
-                 "status": True,
-                 'message':'product added to wishlist'
-                 },201
-        return {
-              'status': False,
-              'message':'product not found'
-        },404
-
-
-class delete_from_wishlist(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('Product_name',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
-    def post(self):
-        data = delete_from_wishlist.parser.parse_args()
-        Product = Wishlist.find_by_name(data['Product_name'])
-        user = User.find_by_username(data['username'])
-        if product:
-            Wishlist.save_to_db(Product)
-            return{
-                 "status": True,
-                 'message':'product removed from wishlist'
-                 },201
-        return {
-              'status': False,
-              'message':'product not in your wishlist'
-        },404
-
-
-class add_to_cart(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('Product_name',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
-    parser.add_argument('username',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
-    def post(self):
-        data = add_to_cart.parser.parse_args()
-        Product = Product.find_by_name(data['Product_name'])
-        user = User.find_by_username(data['username'])
-        if product:
-            cart = Cart(data['Product_name'],product.price,user.id)
-            Cart.save_to_db(cart)
-            return{
-                 "status": True,
-                 'message':'product added to wishlist'
-                 },201
-        return {
-              'status': False,
-              'message':'product not found'
-        },404
-
-class delete_from_cart(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('Product_name',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
-    def post(self):
-        data = delete_from_cart.parser.parse_args()
-        Product = Cart.find_by_name(data['Product_name'])
-        user = User.find_by_username(data['username'])
-        if product:
-            Cart.save_to_db(Product)
-            return{
-                 "status": True,
-                 'message':'product removed from your cart'
-                 },201
-        return {
-              'status': False,
-              'message':'product not in your cart'
-        },404
-
-class buy_product(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('Product_name',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
-    parser.add_argument('username',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
-    parser.add_argument('store_name',
-                        type=str,
-                        required=True,
-                        help="This field cannot be left blank!"
-                        )
-    def post(self):
-        data = buy_product.parser.parse_args()
-        Product = Product.find_by_name(data['Product_name'])
-        user = User.find_by_username(data['username'])
-        store = Store.find_by_name(data['store_name'])
-        inventory = Inventory.find_by_name(data['store_name'])
-        if product:
-            user.account_balance = Integer(user.account_balance)
-            user.account_balance = user.account_balance - Product.price
-            store.account_balance = Integer(store.account_balance)
-            store.account_balance = store.account_balance + Product.price
-
-            inventory.NO_of_products = inventory.NO_of_products - 1
-
-            store.account_balance = string(store.account_balance)
-            user.account_balance = string(user.account_balance)
-
-            return{
-                 "status": True,
-                 'message':'product will be shipped to you soon'
-                 },201
-        return {
-              'status': False,
-              'message':'product not found'
-        },404
-
-
-    
-    
 class Create_store(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('username',
@@ -315,7 +112,6 @@ class Create_store(Resource):
         new_store = Store(data['store_name'],data['description'],data['store_address'],user.id)
         Store.save_to_db(new_store)
         new_inventory = Inventory(data['store_name'],0,new_store.id)
-
         Inventory.save_to_db(new_inventory)
         ki = new_store.json2()
         try:
@@ -326,7 +122,6 @@ class Create_store(Resource):
                  },201
         except:
             return {"message":"little error occured when trying to access db"}
-
 class update_products(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('Product_name',
@@ -349,7 +144,6 @@ class update_products(Resource):
                         required=True,
                         help="This field cannot be left blank!"
                         )
-
     parser.add_argument('Number_available',
                         type=str,
                         required=True,
@@ -371,10 +165,8 @@ class update_products(Resource):
             #file = request.files['inputFile']
             new_product = Product(data['Product_name'],data['description'],data['price'],data['Number_available'],inventory.id)
 
-
             Product.save_to_db(new_product)
             inventory.NO_of_products = inventory.NO_of_products + 1
-
             try:
                 return{
                      "status": True,
@@ -390,11 +182,9 @@ class upload_image(Resource):
                         required=True,
                         help="This field cannot be left blank!"
                         )
-
     @jwt_required()
     def post(self):
         data = upload_image.parser.parse_args()
-
         product = Product.find_by_name(data['Product_name'])
         if product:
             pic = request.files['pic']
@@ -404,36 +194,24 @@ class upload_image(Resource):
             mimetype = pic.mimetype
             if not filename or not mimetype:
                 return 'Bad upload!', 400
-
-
             img = Image(image=pic.read(), name=product.name, mimetype=mimetype,user_id=product.id)
-
-
             Image.save_to_db(img)
             return{
                  "status": True,
                  'message':'image uploaded to product info'
                  },200
-
         return{
              "status": False,
              'message':'product is not in your inventory'
              },404
-
 class First_page(Resource):
     def get(self):
         try:
             return {'stores': list(map(lambda x: x.json(), Store.query.all()))}
         except:
             return {"message":"little error occured when trying to access db"}
-
-
-
-
 class view_store(Resource):
     def get(self,name):
-
-
         store = Store.find_by_name(name)
         if store:
             try:
@@ -444,17 +222,10 @@ class view_store(Resource):
                      },201
             except:
                 return {"message":"little error occured when trying to access db"}
-
-
-
-
-
         return{
              "status": False,
              'message':'store not found'
              },404
-
-
 class view_productimage(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('Product_name',
@@ -462,11 +233,8 @@ class view_productimage(Resource):
                         required=True,
                         help="This field cannot be left blank!"
                         )
-
-
     def post(self):
         data = view_productimage.parser.parse_args()
-
         product = Product.find_by_name(data['Product_name'])
         image = Image.find_by_name(data['Product_name'])
         if product:
